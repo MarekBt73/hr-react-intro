7b3855c6243060c209486ce848b044dc605c137e
Marek Becht <marek.becht@gmail.com>
2021-10-14 23:58:00 +0200

    koniec dnia
    Signed-off-by: Marek Becht <marek.becht@gmail.com>

---
+++ 01/header.js
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return <> 
+    <h1 className="h1" >Menu</h1> 
+    </>
+}
+
+const header = document.querySelector('#app2');
+
+ReactDOM.render(<Header/>, header)
---
+++ 01/main.js
@@ -0,0 +1,18 @@
+
+
+
+const Infos = () =>{
+
+
+return <> 
+<header>Header</header>
+<main><div className="main" id="sooo" >Main</div></main>
+<footer>Footer</footer>
+</>
+
+}
+
+
+
+const appWtrier = document.querySelector('#app');
+ReactDOM.render(<Infos/>,appWtrier);
